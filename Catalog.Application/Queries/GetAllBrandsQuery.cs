7f765e91ff7d808aafba9dc0f8d834bca1c3ae80
Thabang Manaka <allprogrammingtalks@gmail.com>
2026-07-18 10:59:15 +0200

    Added Queries on application

---
+++ Catalog.Application/Queries/GetAllBrandsQuery.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+
+
+namespace Catalog.Application.Queries
+{
+    public partial record GetAllBrandsQuery : IRequest<IList<BrandResponse>>
+    {
+    }
+}
