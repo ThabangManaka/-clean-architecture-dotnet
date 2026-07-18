e23a28a687a06ab6007571c7fb4b3c3600f45de9
Thabang Manaka <allprogrammingtalks@gmail.com>
2026-07-18 11:44:25 +0200

    Added the commands

---
+++ Catalog.Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Application.Commands
+{
+    public record CreateProductCommand : IRequest<ProductResponse>
+    {
+        public string Name { get; init; }
+        public string Summary { get; init; }
+        public string Description { get; init; }
+        public string ImageFile { get; init; }
+        public string BrandId { get; init; }
+        public string TypeId { get; init; }
+        public decimal Price { get; init; }
+    }
+}
