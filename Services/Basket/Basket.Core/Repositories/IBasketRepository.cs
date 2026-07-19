79768f7082ebd4631f8bad018a25a8319771c31c
Thabang Manaka <allprogrammingtalks@gmail.com>
2026-07-19 12:40:54 +0200

    Added the Entities ,Repositories  and redis

---
+++ Services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿using Basket.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Core.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpsertBasket(ShoppingCart shoppingCart);
+        Task DeleteBasket(string userName);
+    }
+}
