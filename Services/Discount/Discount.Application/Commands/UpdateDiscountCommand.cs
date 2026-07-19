d11eaf74b179df4c9dd65893c91129e29ef3ea49
Thabang Manaka <allprogrammingtalks@gmail.com>
2026-07-19 19:10:57 +0200

    Added Application

---
+++ Services/Discount/Discount.Application/Commands/UpdateDiscountCommand.cs
@@ -0,0 +1,7 @@
+﻿using Discount.Application.DTOs;
+using MediatR;
+
+namespace Discount.Application.Commands
+{
+    public record UpdateDiscountCommand(int Id, string ProductName, string Description, int Amount) : IRequest<CouponDto>;
+}
