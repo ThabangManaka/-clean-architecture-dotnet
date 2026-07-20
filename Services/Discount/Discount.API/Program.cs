2b23fbe32e18c7d9d465f0a86fc1179be807a020
Thabang Manaka <allprogrammingtalks@gmail.com>
2026-07-20 10:41:26 +0200

    Register the service on programs

--- Services/Discount/Discount.API/Program.cs
+++ Services/Discount/Discount.API/Program.cs
@@ -1,23 +1,43 @@
+using Catalog.Infrastructure.Settings;
+using Discount.API.Services;
+using Discount.Application.Handlers;
+using Discount.Core.Repositories;
+using Discount.Infrastructure.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Mediatr
+var assemblies = new Assembly[]
+    {
+        Assembly.GetExecutingAssembly(), typeof(CreateDiscountHandler).Assembly
+    };
 
-builder.Services.AddControllers();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddGrpc();
+
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("DatabaseSettings"));
+
+//builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
 {
-    app.MapOpenApi();
-}
+    endpoints.MapGrpcService<DiscountService>();
+});
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
