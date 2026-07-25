e70505aa9386d74d79db4efd00c61ad6a3cc59b9
Thabang Manaka <thabangdansteyn@gmail.com>
2026-07-25 23:05:28 +0200

    Added  outbox Message entity

---
+++ Services/Ordering/Ordering.Application/Constants/OutboxMessageTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Constants
+{
+    public static class OutboxMessageTypes
+    {
+        public const string OrderCreated = "OrderCreated";
+    }
+}
