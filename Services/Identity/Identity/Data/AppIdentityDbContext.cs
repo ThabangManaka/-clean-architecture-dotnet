57b88bfb65435631efb20b20bafcc9b21f260f0c
Thabang Manaka <thabangdansteyn@gmail.com>
2026-07-28 10:56:07 +0200

    Added db context

---
+++ Services/Identity/Identity/Data/AppIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Models;
+
+
+namespace Identity.Data
+{
+    public class AppIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
